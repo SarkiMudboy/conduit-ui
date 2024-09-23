d7e117fcc98785c8da62cd8221651207897b8878
abdul <abdoolihima@gmail.com>
2024-09-23 19:14:13 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import LoginPage from '@/components/LoginPage.vue'
+</script>
+
+<template>
+  <RouterLink to="/login"> Login </RouterLink>
+  <div class="container m-3 py-8">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto explicabo corrupti,
+    similique ducimus quasi repellat fugit alias soluta corporis velit minus ipsa sint itaque dolor
+    placeat cumque est voluptatibus. Expedita.
+  </div>
+  <RouterView />
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container p-10">
+    <p>Welcome to Conduit</p>
+  </div>
+</template>
+
+<style></style>
