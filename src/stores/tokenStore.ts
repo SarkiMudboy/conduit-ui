d648b30f3c9fc963f2af37caf616657822b3ece6
abdul <abdoolihima@gmail.com>
2025-07-04 11:59:30 +0100

    Get token in store from request body

--- src/stores/tokenStore.ts
+++ src/stores/tokenStore.ts
@@ -3,14 +3,26 @@ import { ref } from 'vue'
 
 export const useCSRFTokenStore = defineStore('useCSRFTokenStore', () => {
   const csrfToken = ref('')
-  const getCSRFToken = () => {
-    console.info(document.cookie)
-    csrfToken.value = document.cookie
-      .split(';')
-      .find((row) => row.startsWith('csrftoken='))
-      ?.split('=')[1] as string
+
+  const getCSRFToken = async () => {
+    const baseURL = import.meta.env.VITE_CONDUIT_BASE_URL
+    await fetch(`${baseURL}set-csrf-token/`, {
+      method: 'GET',
+      credentials: 'include'
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        csrfToken.value = data.csrfToken
+      })
+    console.log(csrfToken)
+    //console.info(document.cookie)
+    //csrfToken.value = document.cookie
+    //  .split(';')
+    //  .find((row) => row.startsWith('csrftoken='))
+    //  ?.split('=')[1] as string
   }
   getCSRFToken()
+
   return {
     csrfToken
   }
